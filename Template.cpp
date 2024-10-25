d7c283cbf5b7ec0719e2e2c34c70e427d94aa6fa
Danielle Starlet <dlloyd20@ivytech.edu>
2024-10-25 19:28:24 -0400

    Templates

---
+++ Template.cpp
@@ -0,0 +1,12 @@
+/////////////////////////////////////////////////////////////
+//#  Program name: M01_Lab_2.cpp                          //
+//#  Author: Danielle Lloyd                              //
+//#  Date last updated: 10/24/2024                      // 
+//#  Purpose: Manipulate data involving StereoReceiver //
+////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+#include <limits>
+
+using namespace std;
---
+++ test.py
@@ -0,0 +1,7 @@
+i = 250
+while len(str(i)) > 72:
+ i *= 2
+else:
+ i //= 2
+print(i)
+
